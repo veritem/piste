bbf3e7675f3d6eebde407ab1fab50c8cc442b99e
veritem <mugaboverite@gmail.com>
2021-09-14 22:03:50 +0200

    (feat):working on auth

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,26 @@
 <script>
- import superbase from "$lib/db"
+	import supabase from '$lib/db';
+
+	async function signin() {
+		const { user, session, error } = await supabase.auth.signIn({
+			provider: 'google'
+		});
+
+		console.log({user});
+		console.log({session});
+		console.log({error})
+
+		/* const { error } = await supabase.auth.signOut(); */
+		/* /1* console.log({error}) *1/ */
+	}
 </script>
 
+<svelte:head>
+	<title>Piste</title>
+</svelte:head>
+
+<main class="flex justify-center items-center h-screen w-screen">
+	<button class="bg-blue-500 text-white px-10 py-3 rounded-md" on:click={signin}
+		>continue with google</button
+	>
+</main>
