23d6dc8d49dbfed78b7d1f01ca9208325ccdaa1d
Verite Makuza Mugabo <mugaboverite@gmail.com>
2022-03-06 16:41:02 +0200

    feat: migrate to unocss

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,47 @@
 import vercel from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
+import preprocess from "svelte-preprocess";
+import { extractorSvelte, presetAttributify, presetIcons, presetUno, presetWebFonts } from 'unocss';
+import Unocss from "unocss/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [
-		preprocess({
-			postcss: true
-		})
+		preprocess()
 	],
 
+
 	kit: {
-		adapter: vercel()
+		adapter: vercel(),
+		vite: {
+			plugins: [
+				Unocss({
+					theme: {
+						colors: {
+							primary: '#202020',
+							secondary: '#ffd100',
+							overlay: 'rgba(0, 0, 0,0.5)'
+						},
+						fontFamily: {
+							primary: ['Outfit', 'sans-serif']
+						}
+					},
+					extractors: [extractorSvelte],
+					presets: [
+						presetAttributify(),
+						presetUno(),
+						presetIcons(),
+						presetWebFonts({
+							provider: 'google',
+							fonts: {
+								sans: 'Outfit'
+							}
+						})
+					]
+				})
+			]
+		}
 	}
 };
 
